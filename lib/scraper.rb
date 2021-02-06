85b4f9afadcca13ace85db6d27923404c15b1084
Will Andre <wandre94@gmail.com>
2021-02-06 19:02:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    #binding.pry
+    binding.pry
   end
 
   def print_courses
